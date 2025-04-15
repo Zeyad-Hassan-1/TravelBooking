bdfd1b54b09847325e623c911b7fb7527d32399c
Zeyad-Hassan-1 <studying.zezo@gmail.com>
2025-04-15 17:14:41 +0200

    Enhance booking functionality: add passenger details to booking form and show page, and link flight to booking creation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,19 +9,25 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
+    @flight = Flight.find(params[:flight_id]) if params[:flight_id].present?
   end
 
   def create
     @booking = Booking.new(booking_params)
+    @booking.user_id = current_user.id if user_signed_in?
+    @booking.flight_id = params[:flight_id] || params[:booking][:flight_id] # Fallback
+    @booking.passenger_email = current_user.email if user_signed_in?
+
     if @booking.save
       redirect_to @booking, notice: "Booking was successfully created."
     else
+      puts "Errors: #{@booking.errors.full_messages}"
       render :new
     end
   end
 
   private
   def booking_params
-    params.require(:booking).permit(:user_id, :flight_id)
+    params.require(:booking).permit(:user_id, :flight_id, :passenger_name, :passenger_phone, :passenger_address)
   end
 end
