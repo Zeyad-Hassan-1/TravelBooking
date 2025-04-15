26be17836af85297d5cff3ad31c873330a732f3c
Zeyad-Hassan-1 <studying.zezo@gmail.com>
2025-04-16 01:37:17 +0200

    ignore fav

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "/favicon.ico", to: redirect("/assets/favicon.ico", status: 301)
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :flights, only: [ :index, :show ]
