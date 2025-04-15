44621ff49ee98f516e3bc44314e5b4a21b0caf61
Zeyad-Hassan-1 <studying.zezo@gmail.com>
2025-04-15 19:51:03 +0200

    Implement booking management features: add edit, update, and delete actions; enhance booking index and show views; create a shared booking form partial.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -26,6 +26,26 @@ class BookingsController < ApplicationController
     end
   end
 
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    if @booking.update(booking_params)
+      redirect_to @booking, notice: "Booking was successfully updated."
+    else
+      puts "Errors: #{@booking.errors.full_messages}"
+      render :edit
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_url, notice: "Booking was successfully destroyed."
+  end
+
   private
   def booking_params
     params.require(:booking).permit(:user_id, :flight_id, :passenger_name, :passenger_phone, :passenger_address, :Nkids, :Nadults, :travel_class,  :zip_code)
